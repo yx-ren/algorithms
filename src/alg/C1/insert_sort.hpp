ac8a3b66029fb1e7a7fe0aa8cd7c2fda2c1bf615
yx-ren <yunxiang.r@gmail.com>
2020-04-17 02:22:14 +0800

    template for insert_sort

--- src/alg/C1/insert_sort.hpp
+++ src/alg/C1/insert_sort.hpp
@@ -1,34 +1,37 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-void insert_sort(int arr[], size_t len)
+template<typename T>
+void insert_sort(T& arr, size_t len, bool enbale_dump = false)
 {
-    for (int unsort_index = 1; unsort_index != len; unsort_index++)
-    {
-        int sort_max_index = unsort_index - 1;
+    if (len <= 1)
+        return;
 
-        // find the insert pos
-        int insert_pos = 0; // init the unsort_element was min
-        int unsort_element = arr[unsort_index];
-        for (int sort_index = sort_max_index; sort_index >=0; sort_index--)
+    for (size_t unsort_index = 1; unsort_index != len; unsort_index++)
+    {
+        auto& unsort_element = arr[unsort_index];
+
+        // find insert pos, in sort range, first element
+        size_t insert_pos = 0; // 0 means the unsort_element was min element
+        for (int sort_index = unsort_index - 1; sort_index >= 0; sort_index--)
         {
-            int sort_element = arr[sort_index];
-            if (unsort_element >= sort_element)
+            if (unsort_element > arr[sort_index])
             {
                 insert_pos = sort_index + 1;
                 break;
             }
         }
 
-        // insert arr[index] to {arr[0], arr[index - 1]}
-        int next_insert_element = unsort_element;
-        for (int pos = insert_pos; pos <= unsort_index; pos++)
+        // insert element to sort range(arr[0]~arr[unsort_index])
+        auto next_insert_element = unsort_element;
+        for (size_t pos = insert_pos; pos <= unsort_index; pos++)
             std::swap(next_insert_element, arr[pos]);
 
-        // dump
-        for (int index = 0; index != len; index++)
-            std::cout << arr[index] << (index == unsort_index ? "| " : ", ");
-        std::cout << std::endl;
+        if (enbale_dump)
+        {
+            for (int i = 0; i != len; i++)
+                std::cout << arr[i] << (i == unsort_index ? "|" : ",");
+            std::cout << std::endl;
+        }
     }
 }
--- src/tests/C1/main.cpp
+++ src/tests/C1/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <time.h>
+#include <alg/C1/insert_sort.hpp>
+#include <algorithm>
 
 int main(int argc, const char* argv[])
 {
-    std::cout << "hello, algorithm" << std::endl;
+    srand(0);
+    int arr[] = {7, 4, 1, 6, 7, 9, 0, 0, 0};
+    insert_sort(arr, sizeof(arr) / sizeof(arr[0]), true);
+
+    std::vector<int> vecs;
+    int len = 10;
+    for (int i = 0; i != len; i++)
+        vecs.push_back(rand() % len);
+
+    std::cout << "dump origin random vals:" << std::endl;
+    std::for_each(vecs.begin(), vecs.end(), [&](const int& e){std::cout << e << ",";});
+    std::cout << std::endl;
+
+    insert_sort(vecs, vecs.size(), true);
 
     return 0;
 }
